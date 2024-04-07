789083c261e8123abfd606fca2ba9e63393c1ae2
Alessandro <alessandro.arteaga@inslapineda.cat>
2024-04-07 21:26:06 +0200

    Codigo mejorado mediante herramientas

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import hashlib
 import os
 import time
 from repositorio import *
+
 archivo_usuarios = 'usuarios.txt'
 archivo_libros = 'libros.txt'
 session = 0
+
 try:
     usuario_guardado, contraseña_guardada = verificar_session('usuario.txt')
     if usuario_guardado is not None and contraseña_guardada is not None:
@@ -12,10 +13,12 @@ try:
         session = 1
 except FileNotFoundError:
     pass
+except TypeError:
+    pass
 
 if session == 0: 
     estado = iniciar_sesion(archivo_usuarios)
-    if estado == False:
+    if not estado:
         exit()
 
 time.sleep(2)
@@ -32,38 +35,35 @@ while True:
 
     opcion = input("Seleccione una opción: ")
 
-    if opcion == '1':
-        isbn_a_buscar = input("Ingrese el ISBN del libro: ")
-        libro_encontrado = mostrar_libro(isbn_a_buscar, archivo_libros)
-        if libro_encontrado:
-            print("Información del libro:")
-            print("ISBN:", libro_encontrado[0])
-            print("Título:", libro_encontrado[1])
-            print("Autor:", libro_encontrado[2])
+    try:
+        if opcion == '1':
+            isbn_a_buscar = input("Ingrese el ISBN del libro: ")
+            libro_encontrado = mostrar_libro(isbn_a_buscar, archivo_libros)
+            if libro_encontrado:
+                print("Información del libro:")
+                print("ISBN:", libro_encontrado[0])
+                print("Título:", libro_encontrado[1])
+                print("Autor:", libro_encontrado[2])
+            else:
+                print("Libro no encontrado.")
+        elif opcion == '2':
+            print("Todos los libros:")
+            libros = mostrar_todos_libros(archivo_libros)
+            for libro in libros:
+                print("ISBN:", libro[0])
+                print("Título:", libro[1])
+                print("Autor:", libro[2])
+                print()
+        elif opcion == '3':
+            agregar_libro(archivo_libros)
+        elif opcion == '4':
+            eliminar_libro(archivo_libros)
+        elif opcion == '5':
+            editar_libro(archivo_libros)
+        elif opcion == '6':
+            print("Saliendo del programa...")
+            break
         else:
-            print("Libro no encontrado.")
-
-    elif opcion == '2':
-        print("Todos los libros:")
-        libros = mostrar_todos_libros(archivo_libros)
-        for libro in libros:
-            print("ISBN:", libro[0])
-            print("Título:", libro[1])
-            print("Autor:", libro[2])
-            print()
-
-    elif opcion == '3':
-        agregar_libro(archivo_libros)
-
-    elif opcion == '4':
-        eliminar_libro(archivo_libros)
-
-    elif opcion == '5':
-        editar_libro(archivo_libros)
-
-    elif opcion == '6':
-        print("Saliendo del programa...")
-        break
-
-    else:
-        print("Opción inválida. Por favor, seleccione una opción válida.")
+            print("Opción inválida. Por favor, seleccione una opción válida.")
+    except Exception as e:
+        print("Se produjo un error:", e)
--- repositorio.py
+++ repositorio.py
@@ -4,24 +4,30 @@ def obtener_hash_md5(texto):
     return hashlib.md5(texto.encode()).hexdigest()
 
 def iniciar_sesion(archivo_usuarios):
-    intentos = 3
-    while intentos != 0:
-        usuario = input("Usuario: ")
-        contraseña = input("Contraseña: ")
+    try:
+        intentos = 3
+        while intentos != 0:
+            usuario = input("Usuario: ")
+            contraseña = input("Contraseña: ")
 
-        with open(archivo_usuarios, 'r') as f:
-            for line in f:
-                usuario_guardado, contraseña_guardada = line.strip().split('|')
-                if usuario == usuario_guardado and obtener_hash_md5(contraseña) == contraseña_guardada:
-                    print("Inicio de sesión exitoso.")
-                    with open('usuario.txt', 'a') as f:
-                        f.write(f"{usuario}|{obtener_hash_md5(contraseña)}\n")
-                    return True
-            print("Usuario o contraseña incorrectos. Inténtelo de nuevo. Te quedan:", intentos - 1, "intentos")
-            intentos -= 1
+            with open(archivo_usuarios, 'r') as f:
+                for line in f:
+                    usuario_guardado, contraseña_guardada = line.strip().split('|')
+                    if usuario == usuario_guardado and obtener_hash_md5(contraseña) == contraseña_guardada:
+                        print("Inicio de sesión exitoso.")
+                        with open('usuario.txt', 'a') as f:
+                            f.write(f"{usuario}|{obtener_hash_md5(contraseña)}\n")
+                        return True
+                print("Usuario o contraseña incorrectos. Inténtelo de nuevo. Te quedan:", intentos - 1, "intentos")
+                intentos -= 1
 
-    print("Ha superado el número máximo de intentos. Saliendo del programa.")
-    return False
+        print("Ha superado el número máximo de intentos. Saliendo del programa.")
+        return False
+    except IOError:
+        print("Error al leer el archivo de usuarios.")
+    except Exception as e:
+        print(f"Se produjo un error inesperado: {e}")
+    
 
 def mostrar_libro(isbn, archivo):
     try: 
@@ -30,61 +36,110 @@ def mostrar_libro(isbn, archivo):
                 libro_info = line.strip().split('|')
                 if libro_info[0] == isbn:
                     return libro_info
+            print("El libro con el ISBN", isbn, "no se encontró.")
             return None
-    except FileNotFoundError:
-        print("No se encontró el archivo.")
+    except IOError:
+        print("Error al abrir el archivo:")
+    except Exception as e:
+        print("Se produjo un error inesperado:", e)
 
 def mostrar_todos_libros(archivo):
     try:
-        libros = []
         with open(archivo, 'r') as f:
             for line in f:
-                libros.append(line.strip().split('|'))
-        return libros
-    except FileNotFoundError:
-        print("No se encontró el archivo.")
+                yield line.strip().split('|')
+    except IOError :
+        print(f"Error al abrir el archivo")
+        return None
+    except Exception as e:
+        print(f"Se produjo un error inesperado: {e}")
+        return None
 
 def agregar_libro(archivo):
-    isbn = input("Ingrese el ISBN del libro: ")
-    titulo = input("Ingrese el título del libro: ")
-    autor = input("Ingrese el nombre del autor del libro: ")
-    with open(archivo, 'a') as f:
-        f.write(f"{isbn}|{titulo}|{autor}\n")
-    print("Libro agregado correctamente.")
-
+    try:
+        isbn = input("Ingrese el ISBN del libro: ")
+        titulo = input("Ingrese el título del libro: ")
+        autor = input("Ingrese el nombre del autor del libro: ")
+        
+        # Validar el ISBN antes de agregar el libro
+        if not isbn or not titulo or not autor:
+            print("Por favor, complete todos los campos.")
+            return
+        
+        with open(archivo, 'a') as f:
+            f.write(f"{isbn}|{titulo}|{autor}\n")
+        print("Libro agregado correctamente.")
+    except IOError:
+        print("Error al escribir en el archivo.")
+    except Exception as e:
+        print(f"Se produjo un error inesperado: {e}")
+    
 def eliminar_libro(archivo):
-    isbn = input("Ingrese el ISBN del libro que desea eliminar: ")
-    libros = []
-    with open(archivo, 'r') as f:
-        for line in f:
-            libro_info = line.strip().split('|')
-            if libro_info[0] != isbn:
-                libros.append(libro_info)
-    with open(archivo, 'w') as f:
-        for libro in libros:
-            f.write('|'.join(libro) + '\n')
-    print("Libro eliminado correctamente.")
-
-def editar_libro(archivo):
-    isbn = input("Ingrese el ISBN del libro que desea editar: ")
-    nueva_isbn = input("Ingrese el nuevo ISBN del libro: ")
-    nuevo_titulo = input("Ingrese el nuevo título del libro: ")
-    nuevo_autor = input("Ingrese el nuevo autor del libro: ")
-    libros = []
-    with open(archivo, 'r') as f:
-        for line in f:
-            libro_info = line.strip().split('|')
-            if libro_info[0] == isbn:
-                libros.append([nueva_isbn, nuevo_titulo, nuevo_autor])
+    try:
+        isbn = input("Ingrese el ISBN del libro que desea eliminar: ")
+        with open(archivo, 'r') as f:
+            lines = f.readlines()
+        
+        with open(archivo, 'w') as f:
+            eliminado = False
+            for line in lines:
+                libro_info = line.strip().split('|')
+                if libro_info[0] != isbn:
+                    f.write(line)
+                else:
+                    eliminado = True
+            if eliminado:
+                print("Libro eliminado correctamente.")
             else:
-                libros.append(libro_info)
-    with open(archivo, 'w') as f:
-        for libro in libros:
-            f.write('|'.join(libro) + '\n')
-    print("Libro editado correctamente.")
+                print("No se encontró ningún libro con el ISBN", isbn)
+    except (FileNotFoundError, IOError):
+        print("Error al abrir o manipular el archivo:")
+    except PermissionError:
+        print("Permiso denegado para manipular el archivo.")
+    except Exception as e:
+        print("Se produjo un error inesperado:", e)
+        
+def editar_libro(archivo):
+    try:
+        isbn = input("Ingrese el ISBN del libro que desea editar: ")
+        nueva_isbn = input("Ingrese el nuevo ISBN del libro: ")
+        nuevo_titulo = input("Ingrese el nuevo título del libro: ")
+        nuevo_autor = input("Ingrese el nuevo autor del libro: ")
+
+        # Validación de datos de entrada
+        if not nueva_isbn or not nuevo_titulo or not nuevo_autor:
+            print("Por favor, complete todos los campos.")
+            return
+
+        # Edición del libro directamente en el archivo
+        with open(archivo, 'r+') as f:
+            lines = f.readlines()
+            f.seek(0)
+            for line in lines:
+                libro_info = line.strip().split('|')
+                if libro_info[0] == isbn:
+                    f.write(f"{nueva_isbn}|{nuevo_titulo}|{nuevo_autor}\n")
+                else:
+                    f.write(line)
+            f.truncate()
+        
+        print("Libro editado correctamente.")
+    except (FileNotFoundError, IOError) :
+        print("Error al abrir o manipular el archivo:")
+    except PermissionError:
+        print("Permiso denegado para manipular el archivo.")
+    except Exception as e:
+        print("Se produjo un error inesperado:", e)
+        
+        
 
 def verificar_session(archivo_usuarios):
-    with open(archivo_usuarios, 'r') as f:
-        for line in f:
-            usuario_guardado, contraseña_guardada = line.strip().split('|')
-            return usuario_guardado, contraseña_guardada
+    try:
+        with open(archivo_usuarios, 'r') as f:
+            for line in f:
+                usuario_guardado, contraseña_guardada = line.strip().split('|')
+                return usuario_guardado, contraseña_guardada
+    except FileNotFoundError:  
+        pass
+        
+    
