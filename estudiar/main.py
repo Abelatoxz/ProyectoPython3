15eb4b569fc1d152ef4b326bac6b7ef5b241b1a4
Abelatoxz <92982490+Abelatoxz@users.noreply.github.com>
2024-04-07 22:01:41 +0200

    Delete estudiar directory

--- estudiar/main.py
+++
@@ -1,9 +0,0 @@
-archivo = 'libros.txt'
-libros = []
-with open(archivo, 'r') as f:
-    for line in f:
-        libro_info = line.strip().split(',')
-        if libro_info[0] == isbn
-            return libro_info
-
-print(libros)
